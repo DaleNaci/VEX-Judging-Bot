43b3598ff1609d80e2020b0d64f4c1118804d7fd
DaleNaci <dtn22@njit.edu>
2025-03-09 11:16:13 -0400

    Successfully grabs the json of the first match and prints it to Discord

--- api/robotevents/api_handler.py
+++ api/robotevents/api_handler.py
@@ -3,5 +3,11 @@ import requests
 from .config import API_KEY, BASE_URL
 
 
-def get_match_list(sku: str):
-    pass
+def get_match_list(id: str):
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+    endpoint = f"/events/{id}/divisions/1/matches?team=141070"
+
+    response = requests.get(BASE_URL+endpoint, headers=headers)
+    response.raise_for_status()
+
+    return response.json()
--- cogs/matchlist.py
+++ cogs/matchlist.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from api.robotevents.api_handler import get_match_list
+
 
 class MatchList(commands.Cog):
     def __init__(self, bot):
@@ -9,11 +11,16 @@ class MatchList(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.author.bot:
-            return
-        
-        pass
+        if message.content.lower() == "matchlist":
+            await message.channel.send(get_match_list(58565))
 
+
+    @commands.command()
+    async def matchlist(self, ctx):
+        match_list_json = get_match_list(58565)
+        first_match_json = match_list_json["data"][0]
+
+        await ctx.send(first_match_json)
 
     
 async def setup(bot):
